5720795c36f8475a046f8328cafd2415fe1b5425
AlexBoynich <101637186+AlexBoynich@users.noreply.github.com>
2023-11-23 16:48:46 +0700

    end

--- src/components/AccordionList.vue
+++ src/components/AccordionList.vue
@@ -23,18 +23,46 @@
             :checkedBoxes="checkedBoxes"
             :list="list"
         />
+
+        <button @click="addElement(list)">Добавить элемент</button>
+        <button @click="removeElement(list)">Удалить элемент</button>
+
+        <br/>
+        <br/>
+        <br/>
+
         <AccordionItem
-            :list="[{title: 'title', id: 1000}]"
-            :defaultDetails="defaultDetails"
+            :list="[{title: 'Accordion', id: '100'}]"
         >
+        <div>
+            <input type="radio" id="one" value=true v-model="secondRadioButton">
+                <label for="one">Открыть все</label>
+            <input type="radio" id="two" value=false v-model="secondRadioButton">
+                <label for="two">Закрыть все</label>
+        </div>
+        <div                 
+            v-for="itemSecond in secondList" 
+            :key="itemSecond.id" type="checkbox" 
+        >
+            <input 
+                type="checkbox"
+                :id="itemSecond.id" 
+                :value="itemSecond.id"
+                v-model="checkedSecondBoxes"
+            >
+            <label :for="itemSecond.id">{{itemSecond.id}}</label>
+        </div>
             <AccordionItem 
-                :defaultDetails="defaultDetails"
-                :checkedBoxes="checkedBoxes"
-                :list="list"
+                :defaultDetails="defaultSecondDetails"
+                :checkedBoxes="checkedSecondBoxes"
+                :list="secondList"
             />
+
+            <button @click="addElement(secondList)">Добавить элемент</button>
+            <button @click="removeElement(secondList)">Удалить элемент</button>
+
+
         </AccordionItem>
-        <button @click="addElement">Добавить элемент</button>
-        <button @click="removeElement">Удалить элемент</button>
     </div>
 </template>
 
@@ -46,25 +74,40 @@ import AccordionItem from './AccordionItem.vue';
 
     let defaultDetails = ref(false);
 
+    const checkedBoxes = ref([])
+
+    const checkedSecondBoxes = ref([])
+
+    let secondRadioButton = ref('false')
+
+    let defaultSecondDetails = ref(false);
+
     const list = ref([
         {title: 'title', id: 1},
         {title: 'title', id: 2},
         {title: 'title', id: 3},
         {title: 'title', id: 4},
-])
+    ])
 
-    const checkedBoxes = ref([])
+    const secondList = ref([
+        {title: 'Accordion', id: 1},
+    ])
+
     
     watch(radioButton, (newValue) => {
       if (newValue =='true') return defaultDetails.value =  true
       else return defaultDetails.value =  false
     });
+    watch(secondRadioButton, (newValue) => {
+      if (newValue =='true') return defaultSecondDetails.value =  true
+      else return defaultSecondDetails.value =  false
+    });
 
-    function addElement () {
-        list.value.push({title: 'title', id: list.value.length + 1})
+    function addElement (a) {
+        a.push({title: 'title', id: a.length + 1})
     }
-    function removeElement () {
-        list.value.pop()
+    function removeElement (a) {
+        a.pop()
     }
 
 </script>
